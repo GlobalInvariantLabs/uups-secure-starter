0abdec10387083410043fbdac49ea2c3b15f8e3c
Global Invariant Labs <u4197266860@gmail.com>
2026-01-06 16:18:44 +0100

    Update TestHarnessV2.sol

--- contracts/harness/TestHarnessV2.sol
+++ contracts/harness/TestHarnessV2.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import {TestHarnessV1} from "./TestHarnessV1.sol";
 
+/// @notice V2 extends V1 and adds a version() function to validate upgrade wiring end-to-end.
 contract TestHarnessV2 is TestHarnessV1 {
     function version() external pure returns (uint256) {
         return 2;
